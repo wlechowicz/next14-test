7da0d859e5a6afdf2bfc3b34f247d87eaf3ae027
Wojciech Lechowicz <wojciech.lechowicz@intive.com>
2023-11-14 00:49:52 +0100

    chore: next 14.0.3-canary.6

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    ppr: true,
+    ppr: false,
   },
 };
 
 module.exports = nextConfig;
+
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+//   openAnalyzer: false,
+// });
+
+// module.exports = withBundleAnalyzer(nextConfig);
